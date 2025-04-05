cbcca2efa86a180627cab1e8ac3f34ffab389769
webQbe <webcubesolutionslk@gmail.com>
2025-04-05 15:03:51 +0530

    Created, styled & rendered component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Component1 from './component/Component1'
 
 function App() {
 
   return (
       <div className="App">
-        <h1>Welcome</h1>
+        <Component1 />
       </div>
   )
 }

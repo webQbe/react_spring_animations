277708f601646ab5a340cfe3e95e628b7c570d0d
webQbe <webcubesolutionslk@gmail.com>
2025-04-06 19:21:40 +0530

    Animated number from 0-10 & displayed it in styled counter

--- src/components/Component1.jsx
+++ src/components/Component1.jsx
@@ -4,7 +4,7 @@ import { useSpring, animated } from '@react-spring/web'
 export default function Component1() {
 
   // Define animation style object
-  const c1Style = useSpring({
+  const animStyle = useSpring({
     from: {
             opacity: 0,                     // Start from completely transparent
             transform: 'translateY(-500px)' // Start 500px above its normal position
@@ -16,10 +16,26 @@ export default function Component1() {
         },   
   })
 
+  /* Define 0 - 10 Count-Up Animation  */
+  const animStyle2 = useSpring({
+    from: { number: 0 }, 
+    to: { number: 10 },
+    config: { duration: 2000 }              // take 2 seconds to complete the animation
+  })
+
+  /* Styling for <animated.h1> */
+  const counter = {
+    background: '#333',
+    textAlign: 'center',
+    width: '100px',
+    borderRadius: '50%',
+    margin: '1rem auto'
+  }
+
   return (
-    <animated.div   // Special div component from React Spring that accepts animated styles
+    <animated.div     // Special div component from React Spring that accepts animated styles
       style={{
-        ...c1Style, // Accept the animation style object in as a prop
+        ...animStyle, // Accept the animation style object in as a prop
         /* Merge in regular CSS props */
         background: 'steelblue',
         color: 'white',
@@ -33,6 +49,21 @@ export default function Component1() {
         magni itaque laudantium necessitatibus reprehenderit? Officiis quaerat
         consequuntur maxime placeat!
       </p>
+
+  
+      <div>
+          <animated.h1 /* This tag makes sure the number value updates as part of the animation */
+              style={ counter } // Style for for the tag
+          >
+            { // Access count-up animation
+              animStyle2.number.to( 
+                        // Convert to an integer for display
+                        n => n.toFixed(0) 
+                      ) 
+            }
+          </animated.h1>
+      </div>
+
     </animated.div>
   )
 }
